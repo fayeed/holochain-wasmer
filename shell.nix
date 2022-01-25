c3e48197cdbe776d316d9e821f6b56efb875b933
Stefan Junker <mail@stefanjunker.de>
2022-01-25 01:28:40 +0100

    reorganize test and CI setup

--- shell.nix
+++ shell.nix
@@ -7,4 +7,15 @@ let
     };
   };
 
-in holonix.main
+in
+holonix.pkgs.mkShell {
+  inputsFrom = [ holonix.main ];
+  shellHook = ''
+    export RUST_BACKTRACE=full
+    export WASMER_BACKTRACE=1
+  '';
+
+  packages = with holonix.pkgs; [
+    nixpkgs-fmt
+  ];
+}
--- test.sh
+++ test.sh
@@ -1,13 +1,14 @@
+#!/bin/sh
 export RUST_BACKTRACE=full
 export WASMER_BACKTRACE=1
+
+set -e
+
 cargo fmt
 ( cd test && cargo fmt )
 ( cd crates/guest && cargo fmt )
 
-# tests the root workspace that doesn't include any wasm code
+# tests the root workspace that includes all wasm code
 cargo test -- --nocapture
 
 # cargo build --release --manifest-path test/test_wasm/Cargo.toml --target wasm32-unknown-unknown -Z unstable-options
-
-# build wasm and run the "full" tests
-cargo test --manifest-path test/Cargo.toml ${1} -- --nocapture
